f3717dd7ed0a1336c18ebc6ceaaf6c5754410364
danila nikitin <danila.n2015@yandex.ru>
2024-02-19 17:44:20 +0300

    add Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,6 +3,47 @@
  Напишите программу, которая покажет количество чётных чисел в массиве.
 */
 
-void Main(){
-    
+void Main()
+{   
+    int arraySize = 10;
+    int minNumber = 100;
+    int maxNumber = 999;
+    int[] numbers = GetRandomArray(arraySize, minNumber, maxNumber);
+    PrintArray(numbers);
+    int evenCount = CalcCountEventNumbers(numbers);
+    System.Console.WriteLine($"Количество четных чисел в массиве = {evenCount}");
+
 }
+
+void PrintArray(int[] numbers)
+{
+  System.Console.WriteLine(string.Join(" ", numbers));
+}
+
+int[] GetRandomArray(int arraySize, int minNumber, int maxNumber)
+{
+    int[] array = new int[arraySize];
+    Random random = new Random();
+    int count = 0;
+    for (int i = 0; i < arraySize; i++)
+    {
+        int number = random.Next(minNumber, maxNumber);
+        if(number % 2 == 0)
+            count ++;
+        array[i] = number;
+    }
+
+    return array;
+}
+
+int CalcCountEventNumbers(int[] numbers)
+{   
+    int count = 0;
+    foreach(int number in numbers)
+        if(number % 2 == 0)
+            count ++;
+
+    return count;
+}
+
+Main();
